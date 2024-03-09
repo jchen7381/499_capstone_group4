09d481319e3fd817f540927f1224abec83fa7b0c
Evan <evanwu35@gmail.com>
2024-03-08 21:34:26 -0500

    updates

--- static/js/index.js
+++ static/js/index.js
@@ -1,41 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const firebaseConfig = {
-      apiKey: "AIzaSyCc96lDeYpM5AvvevoLOxR_ZjvfiLeiCL4",
-      authDomain: "capstone-user-database.firebaseapp.com",
-      databaseURL: "https://capstone-user-database-default-rtdb.firebaseio.com",
-      projectId: "capstone-user-database",
-      storageBucket: "capstone-user-database.appspot.com",
-      messagingSenderId: "203034257754",
-      appId: "1:203034257754:web:f1a86d122e7399f5e5ab63"
-    };
-    firebase.initializeApp(firebaseConfig);
-    
-    const registerButton = document.getElementById("register-button");
-    const loginButton = document.getElementById("login-button");
-    
-    registerButton.addEventListener("click", function () {
-        const email = document.getElementById("register-email").value;
-        const password = document.getElementById("register-password").value;
-    
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(function (userCredential) {
-                console.log("User registered successfully");
-            })
-            .catch(function (error) {
-                console.error(error.message);
-            });
-    });
-    
-    loginButton.addEventListener("click", function () {
-        const email = document.getElementById("login-email").value;
-        const password = document.getElementById("login-password").value;
-    
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(function (userCredential) {
-                console.log("User logged in successfully");
-            })
-            .catch(function (error) {
-                console.error(error.message);
-            });
-    });
-    });
+window.onload = function() {
+    // Show the login tab by default
+    showTab('login');
+}
+
+function showTab(tabName) {
+    // Get all elements with class="tab" and hide them
+    var tabs = document.getElementsByClassName("tab");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+
+    // Get all elements with class="tab-button" and remove the class "active"
+    var tabButtons = document.getElementsByClassName("tab-button");
+    for (var i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
