a68e8bf82fa5ea9096a72042d4f9bb4a5ee70aa0
Evan <evanwu35@gmail.com>
2024-03-19 23:40:10 -0400

    routing react through flask

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../react-app/dist')
 
-@app.route('/')
-def home():
-    return render_template('login.html')
-
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
