eb0959e716443229d4bd2f7d2a24a1c70b48adf9
Evan <evanwu35@gmail.com>
2024-04-16 02:25:47 -0400

    changed styling for upload, added auto open window when starting frontend

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -4,10 +4,7 @@ const Home = () => {
     <div id='home'>
       <h1>Home Page</h1>
       <Link to="/login">
-        <button>Return to Login</button>
-      </Link>
-      <Link to="/dashboard">
-        <button>Go to Dashboard</button>
+        <button>Go to Login</button>
       </Link>
     </div>
   );
