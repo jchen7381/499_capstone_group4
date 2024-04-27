28ec346f9bba2e054e9c6d8469d2892b718854c8
Evan <evanwu35@gmail.com>
2024-04-27 14:33:49 -0400

    added README, changed code formatting and added fonts to css files

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 499_capstone_group4
+
+## Setup
+
+- Use the command `git pull` to retrieve the latest commits.
+- Start the backend first to enable functionalities in the frontend.
+
+## Backend
+
+- Install the Supabase library using the command `pip install supabase`.
+- To start the server:
+  - Navigate to the backend directory with `cd backend`.
+  - If you are using Windows, use `python app.py`.
+  - If you are using macOS, use `python3 app.py`.
+
+## Frontend
+
+- Use the command `npm i` to install all node dependencies.
+- To start the frontend:
+  - Navigate to the frontend directory with `cd frontend`.
+  - Run `npm run dev`.
--- frontend/src/components/PdfViewer/PdfViewer.jsx
+++ frontend/src/components/PdfViewer/PdfViewer.jsx
@@ -37,27 +37,27 @@ const PdfViewer = ({url, resetInterface}) => {
 
 	useEffect(() => {
 		if (pdfContainerRef.current) {
-		const boundingRect = pdfContainerRef.current.getBoundingClientRect();
-		setPdfWidth(boundingRect.width);
-		setPdfHeight(boundingRect.height);
+			const boundingRect = pdfContainerRef.current.getBoundingClientRect();
+			setPdfWidth(boundingRect.width);
+			setPdfHeight(boundingRect.height);
 		}
 	}, [pdfContainerRef.current]);
 
 	useEffect(() => {
 		const observer = new MutationObserver(mutations => {
-		mutations.forEach(mutation => {
-			if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-			const boundingRect = pdfContainerRef.current.getBoundingClientRect();
-			setPdfWidth(boundingRect.width);
-			setPdfHeight(boundingRect.height);
-			setScale(1);
-			}
-		});
+			mutations.forEach(mutation => {
+				if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+					const boundingRect = pdfContainerRef.current.getBoundingClientRect();
+					setPdfWidth(boundingRect.width);
+					setPdfHeight(boundingRect.height);
+					setScale(1);
+				}
+			});
 		});
 
 		const config = { attributes: true, attributeFilter: ['style'] };
 		if (pdfContainerRef.current) {
-		observer.observe(pdfContainerRef.current, config);
+			observer.observe(pdfContainerRef.current, config);
 		}
 
 		return () => observer.disconnect(); 
@@ -66,14 +66,14 @@ const PdfViewer = ({url, resetInterface}) => {
 	// Go to previous page
 	const handlePreviousPage = () => {
 		if (currentPage > 1) {
-		setCurrentPage(currentPage - 1);
+			setCurrentPage(currentPage - 1);
 		}
 	};
 
 	// Go to next page
 	const handleNextPage = () => {
 		if (currentPage < numPages) {
-		setCurrentPage(currentPage + 1);
+			setCurrentPage(currentPage + 1);
 		}
 	};
 
@@ -81,11 +81,11 @@ const PdfViewer = ({url, resetInterface}) => {
 	const handleInputChange = (event) => {
 		let value = parseInt(event.target.value, 10);
 		if (isNaN(value)) {
-		value = '';
+			value = '';
 		} else if (value < 1) {
-		value = 1;
+			value = 1;
 		} else if (value > numPages) {
-		value = numPages;
+			value = numPages;
 		}
 		setInputPage(value);
 		setCurrentPage(value);
@@ -100,17 +100,17 @@ const PdfViewer = ({url, resetInterface}) => {
 		const pdfContentElement = pdfContainerRef.current;
 		const scale = 3;
 		const options = {
-		scale: scale,
-		willReadFrequently: true
+			scale: scale,
+			willReadFrequently: true
 		};
 
 		options.willReadFrequently = true;
 
 		html2canvas(pdfContentElement, options).then((canvas) => {
-		canvas.getContext('2d').willReadFrequently = true;
-		const imgData = canvas.toDataURL('image/png');
-		setScreenshotImage(imgData);
-		setImageModalVisible(true);
+			canvas.getContext('2d').willReadFrequently = true;
+			const imgData = canvas.toDataURL('image/png');
+			setScreenshotImage(imgData);
+			setImageModalVisible(true);
 		});
 	};
 
@@ -128,56 +128,56 @@ const PdfViewer = ({url, resetInterface}) => {
 	// Function for sending to backend (after clicking on Send to AI)
 	const handleSend = () => {
 		if (selectedSubject === "Custom" && !customQuery) {
-		alert("Please enter your query.");
-		return;
+			alert("Please enter your query.");
+			return;
 		}
 
 		if (!selectedSubject && selectedSubject !== "Custom") {
-		alert("Please select a subject before sending to AI.");
-		return;
+			alert("Please select a subject before sending to AI.");
+			return;
 		}
 
 		if (cropperRef.current && cropperRef.current.cropper) {
-		const croppedCanvas = cropperRef.current.cropper.getCroppedCanvas();
-		if (croppedCanvas) {
-			const croppedImage = croppedCanvas.toDataURL('image/png');
-			setAiOutput({ result: 'Processing...', image: croppedImage });
-			setProcessing(true);
-			const base64Image = croppedImage.replace(/^data:image\/(png|jpg);base64,/, '');
+			const croppedCanvas = cropperRef.current.cropper.getCroppedCanvas();
+			if (croppedCanvas) {
+				const croppedImage = croppedCanvas.toDataURL('image/png');
+				setAiOutput({ result: 'Processing...', image: croppedImage });
+				setProcessing(true);
+				const base64Image = croppedImage.replace(/^data:image\/(png|jpg);base64,/, '');
 
-			const imageData = {
-			image: base64Image,
-			subject: selectedSubject,
-			customQuery: selectedSubject === "Custom" ? customQuery : null
-			};
+				const imageData = {
+					image: base64Image,
+					subject: selectedSubject,
+					customQuery: selectedSubject === "Custom" ? customQuery : null
+				};
 
-			fetch('http://127.0.0.1:5000/process', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(imageData)
-			})
-			.then(response => response.json())
-			.then(data => {
-			console.log('Response from backend:', data);
-			setAiOutput({ result: data.result, image: croppedImage });
-			setProcessing(false);
-			setSelectedSubject('');
-			setOriginalImage(croppedImage);
-			setOriginalSubject(selectedSubject);
-			setCustomQuery('');
-			if (selectedSubject === "Custom") {
-				setOriginalCustomQuery(customQuery);
+				fetch('http://127.0.0.1:5000/process', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(imageData)
+				})
+				.then(response => response.json())
+				.then(data => {
+					console.log('Response from backend:', data);
+					setAiOutput({ result: data.result, image: croppedImage });
+					setProcessing(false);
+					setSelectedSubject('');
+					setOriginalImage(croppedImage);
+					setOriginalSubject(selectedSubject);
+					setCustomQuery('');
+					if (selectedSubject === "Custom") {
+						setOriginalCustomQuery(customQuery);
+					}
+				})
+				.catch(error => {
+					console.error('Error uploading image:', error);
+					setProcessing(false);
+					setSelectedSubject('');
+					setCustomQuery('');
+				});
 			}
-			})
-			.catch(error => {
-			console.error('Error uploading image:', error);
-			setProcessing(false);
-			setSelectedSubject('');
-			setCustomQuery('');
-			});
-		}
 		}
 		setImageModalVisible(false);
 	};
@@ -199,12 +199,12 @@ const PdfViewer = ({url, resetInterface}) => {
 		const scaleHeight = pdfHeight / 792;
 		const computedScale = Math.min(scaleWidth, scaleHeight);
 		if (computedScale !== scale) {
-		setScale(computedScale);
+			setScale(computedScale);
 		}
 		return (
-		<Document file={pdfUrl} onLoadSuccess={handleLoadSuccess}>
-			<Page pageNumber={currentPage} width={612} scale={computedScale} />
-		</Document>
+			<Document file={pdfUrl} onLoadSuccess={handleLoadSuccess}>
+				<Page pageNumber={currentPage} width={612} scale={computedScale} />
+			</Document>
 		);
 	};
 
@@ -222,35 +222,35 @@ const PdfViewer = ({url, resetInterface}) => {
 	// Function for regenerating output
 	const handleRegenerate = () => {
 		if (originalImage && (originalSubject || customQuery)) {
-		setAiOutput({ result: 'Processing...', image: originalImage });
-		setProcessing(true);
-	
-		setTimeout(() => {
-			const base64Image = originalImage.replace(/^data:image\/(png|jpg);base64,/, '');
-			const imageData = {
-			image: base64Image,
-			subject: originalSubject,
-			customQuery: originalSubject === "Custom" ? originalCustomQuery : null
-			};
-	
-			fetch('http://127.0.0.1:5000/process', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(imageData)
-			})
-			.then(response => response.json())
-			.then(data => {
-			console.log('Response from backend:', data);
-			setAiOutput({ result: data.result, image: originalImage });
-			setProcessing(false);
-			})
-			.catch(error => {
-			console.error('Error re-running AI processing:', error);
-			setProcessing(false);
-			});
-		}, 2000);
+			setAiOutput({ result: 'Processing...', image: originalImage });
+			setProcessing(true);
+		
+			setTimeout(() => {
+				const base64Image = originalImage.replace(/^data:image\/(png|jpg);base64,/, '');
+				const imageData = {
+					image: base64Image,
+					subject: originalSubject,
+					customQuery: originalSubject === "Custom" ? originalCustomQuery : null
+				};
+		
+				fetch('http://127.0.0.1:5000/process', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(imageData)
+				})
+				.then(response => response.json())
+				.then(data => {
+					console.log('Response from backend:', data);
+					setAiOutput({ result: data.result, image: originalImage });
+					setProcessing(false);
+				})
+				.catch(error => {
+					console.error('Error re-running AI processing:', error);
+					setProcessing(false);
+				});
+			}, 2000);
 		}
 	};  
 
@@ -260,20 +260,20 @@ const PdfViewer = ({url, resetInterface}) => {
 			{imageModalVisible ? (
 			<>
 				<select className='dropdown-box' onChange={(e) => setSelectedSubject(e.target.value)}>
-				<option className='dropdown-box' value="">Select Subject</option>
-				<option className='dropdown-box' value="Math">Math</option>
-				<option className='dropdown-box' value="CS">Computer Science</option>
-				<option className='dropdown-box' value="English">English</option>
-				<option className='dropdown-box' value="Other">Other</option>
-				<option className='dropdown-box' value="Custom">Custom</option>
+					<option className='dropdown-box' value="">Select Subject</option>
+					<option className='dropdown-box' value="Math">Math</option>
+					<option className='dropdown-box' value="CS">Computer Science</option>
+					<option className='dropdown-box' value="English">English</option>
+					<option className='dropdown-box' value="Other">Other</option>
+					<option className='dropdown-box' value="Custom">Custom</option>
 				</select>
 				{selectedSubject === "Custom" && (
-				<input className='input-box'
-					type="text"
-					placeholder="Enter your query"
-					value={customQuery}
-					onChange={(e) => setCustomQuery(e.target.value)}
-				/>
+					<input className='input-box'
+						type="text"
+						placeholder="Enter your query"
+						value={customQuery}
+						onChange={(e) => setCustomQuery(e.target.value)}
+					/>
 				)}
 				<button onClick={handleSend} disabled={processing}>Send to AI</button>
 				<button onClick={handleCloseSnip}>Close Snip</button>
@@ -281,19 +281,19 @@ const PdfViewer = ({url, resetInterface}) => {
 			) : (
 			<>
 				<button onClick={handlePreviousPage} disabled={currentPage === 1}>
-				Previous
+					Previous
 				</button>
 				<span>Page </span>
 				<input className='input-box'
-				type="number"
-				min="1"
-				max={numPages || ''}
-				value={inputPage}
-				onChange={handleInputChange}
+					type="number"
+					min="1"
+					max={numPages || ''}
+					value={inputPage}
+					onChange={handleInputChange}
 				/>
 				<span> of {numPages}</span>
 				<button onClick={handleNextPage} disabled={currentPage === numPages}>
-				Next
+					Next
 				</button>
 				<button onClick={handleScreenshot}>Snip</button>
 				<button onClick={handleClosePDF}>Close PDF</button>
@@ -302,48 +302,48 @@ const PdfViewer = ({url, resetInterface}) => {
 		</div>
 		<div className="pdf-viewer-container" ref={pdfContainerRef}>
 			<div className="pdf-content">
-			{renderPdf()}
+				{renderPdf()}
 			</div>
 			{imageModalVisible && (
-			<div className="screenshot-modal">
-				<Cropper
-				ref={cropperRef}
-				src={screenshotImage}
-				guides={true}
-				/>
-			</div>
+				<div className="screenshot-modal">
+					<Cropper
+					ref={cropperRef}
+					src={screenshotImage}
+					guides={true}
+					/>
+				</div>
 			)}
 		</div>
 		<div className="outputs">
 			<div className="snip-preview">
-			<h3 className="output-header">Snip Preview</h3>
-			{aiOutput.image && 
-				<img 
-				src={aiOutput.image} 
-				alt="Snipped" 
-				style={{ maxWidth: '100%', maxHeight: '100%' }} 
-				onClick={() => handlePreviewImageClick(aiOutput.image)} 
-				/>
-			}
+				<h3 className="output-header">Snip Preview</h3>
+				{aiOutput.image && 
+					<img 
+					src={aiOutput.image} 
+					alt="Snipped" 
+					style={{ maxWidth: '100%', maxHeight: '100%' }} 
+					onClick={() => handlePreviewImageClick(aiOutput.image)} 
+					/>
+				}
 			</div>
 			<div className="ai-output">
-			<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-				<h3 className="ai-output-header">AI Output</h3>
-				<div>
-				<button onClick={handleCopyToClipboard}>Copy to Clipboard</button>
-				<button onClick={handleRegenerate}>Regenerate Output</button>
+				<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+					<h3 className="ai-output-header">AI Output</h3>
+					<div>
+						<button onClick={handleCopyToClipboard}>Copy to Clipboard</button>
+						<button onClick={handleRegenerate}>Regenerate Output</button>
+					</div>
 				</div>
-			</div>
-			<div className="ai-output-text">
-				{aiOutput.result.split('\n').map((paragraph, index) => (
-				<p key={index}>{paragraph}</p>
-				))}
-			</div>
+				<div className="ai-output-text">
+					{aiOutput.result.split('\n').map((paragraph, index) => (
+						<p key={index}>{paragraph}</p>
+					))}
+				</div>
 			</div>
 		</div>
 		{previewImageVisible && (
 			<div className="preview-image-modal" onClick={handleClosePreviewImage}>
-			<img src={enlargedImage} alt="Enlarged Preview" />
+				<img src={enlargedImage} alt="Enlarged Preview" />
 			</div>
 		)}
 		</div>
