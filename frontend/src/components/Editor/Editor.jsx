f4821b6a588f975e2c306759a0302bfccff83e65
Evan <evanwu35@gmail.com>
2024-05-01 19:49:43 -0400

    Revert "changed the input box styling"
    This reverts commit 0817621bb47275d950e7a4d91502131697be6abc.

--- frontend/src/components/Editor/Editor.jsx
+++ frontend/src/components/Editor/Editor.jsx
@@ -1,107 +1,111 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import './Editor.css';
+import './Editor.css'
 import { saveAs } from 'file-saver';
 import { pdfExporter } from 'quill-to-pdf';
 
-function Editor({ editor_id, workspace_id, title }) {
+function Editor({editor_id, workspace_id, title}){
     const [value, setValue] = useState('');
     const [workspaceTitle, setWorkspaceTitle] = useState(title);
-    const [isInputFocused, setIsInputFocused] = useState(false); 
 
-    useEffect(() => {
-        if (!value) {
-            editorGet();
+    console.log(editor_id)
+    useEffect(()=>{
+        if (!value){
+            editorGet()
         }
-    }, []);
-
-    useEffect(() => {
-        if (value) {
-            save();
+    }, [])
+    //Change saving method
+    useEffect(()=>{
+        if (value){
+            save()
         }
-    }, [value]);
+    }, [value])
 
     useEffect(() => {
         workspaceSave();
     }, [workspaceTitle]);
 
-    async function save() {
-        try {
+
+    async function save(){
+        try{    
             const res = await fetch('http://127.0.0.1:5000/editor/save', {
                 method: 'POST',
-                credentials: 'include',
+                credentials:'include',
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ 'editor_id': editor_id, 'data': value })
-            });
-            const ret = await res.json();
-        } catch (error) {
-            console.error(error);
+            body: JSON.stringify({'editor_id': editor_id, 'data': value})
+            })
+            const ret = await res.json()
+        }
+        catch{
+
         }
     }
 
-    async function workspaceSave() {
-        try {
+    async function workspaceSave(){
+        try{    
             const res = await fetch('http://127.0.0.1:5000/workspace/save', {
                 method: 'POST',
-                credentials: 'include',
+                credentials:'include',
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ 'workspace_id': workspace_id, 'workspace_title': workspaceTitle })
-            });
-            const ret = await res.json();
-        } catch (error) {
-            console.error(error);
+            body: JSON.stringify({'workspace_id': workspace_id, 'workspace_title': workspaceTitle})
+            })
+            const ret = await res.json()
+        }
+        catch{
+
         }
     }
 
-    async function editorGet() {
-        try {
+    async function editorGet(){
+        try{    
             const res = await fetch('http://127.0.0.1:5000/editor/get', {
                 method: 'POST',
-                credentials: 'include',
+                credentials:'include',
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ 'editor_id': editor_id })
-            });
-            const ret = await res.json();
-            if (res.ok) {
-                const data = ret[0].data.ops;
-                setValue(data);
+            body: JSON.stringify({'editor_id':editor_id})
+            })
+            const ret = await res.json()
+            if (res.ok){
+                const data = ret[0].data.ops
+                setValue(data)
             }
-        } catch (error) {
-            console.error(error);
+        }
+        catch{
+
         }
     }
 
     function handleChange(content, delta, source, editor) {
-        setValue(editor.getContents());
+        setValue(editor.getContents())
     }
 
     async function downloadPdf() {
         const pdf = await pdfExporter.generatePdf(value);
         saveAs(pdf, `${workspaceTitle}.pdf`);
     }
-
+    
     const changeWorkspaceTitle = (newWorkspaceTitle) => {
         setWorkspaceTitle(newWorkspaceTitle);
-    };
-
+    }
+    
     const modules = {
         toolbar: [
-            [{ 'font': [] }, { 'size': [] }],
-            [{ 'script': 'sub' }, { 'script': 'super' }],
+            [{ 'font': [] }, { 'size': []}],
+            [{ 'script': 'sub'}, { 'script': 'super' }],
             ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' },
-            { 'indent': '-1' }, { 'indent': '+1' }, 'direction'],
-            [{ 'align': [] }],
+            [{'list': 'ordered'}, {'list': 'bullet'}, 
+             {'indent': '-1'}, {'indent': '+1'}, 'direction'],
+            [{'align': []}],
             ['link', 'image', 'video'],
             ['clean']
         ],
@@ -115,26 +119,17 @@ function Editor({ editor_id, workspace_id, title }) {
         }
     };
 
-    return (
+    return(
         <div className='text-editor-container'>
-            <div id='text-editor-navbar'>
-                <input
-                    className={`input-box ${isInputFocused ? 'focused' : ''}`} 
-                    type="text"
-                    placeholder="Enter file name"
-                    id = "editor-input"
-                    value={workspaceTitle}
-                    onChange={(e) => changeWorkspaceTitle(e.target.value)}
-                    onFocus={() => setIsInputFocused(true)} 
-                    onBlur={() => setIsInputFocused(false)} 
-                />
+            <div id = 'text-editor-navbar'>
+                <input className='input-box' type="text" placeholder="Enter file name" value={workspaceTitle} onChange={(e) => changeWorkspaceTitle(e.target.value)} />
                 <button onClick={downloadPdf}>Download File</button>
             </div>
             <div className='quill-container'>
-                <ReactQuill theme="snow" value={value} onChange={handleChange} modules={modules} />
+                <ReactQuill theme="snow" value={value} onChange={handleChange} modules ={modules}/>
             </div>
         </div>
-    );
+    )
 }
 
-export default Editor;
+export default Editor
